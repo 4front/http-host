f8cb22f7328016242c78263611e6b27113be13c9
David Von Lehman <david.vonlehman@gmail.com>
2015-04-08 15:08:16 -0700

    Improvements to error handling

--- test/error-fallback.js
+++ test/error-fallback.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var supertest = require('supertest');
 var _ = require('lodash');
 var assert = require('assert');
@@ -9,7 +10,7 @@ require('simple-errors');
 
 describe('errorFallback', function() {
   var self;
-  
+
   beforeEach(function() {
     self = this;
 
@@ -26,16 +27,50 @@ describe('errorFallback', function() {
       next(self.error);
     });
 
-    this.server.use(errorFallback());
+    this.options = {};
+    this.server.use(errorFallback(this.options));
   });
 
   it('renders default error page', function(done) {
     supertest(this.server)
       .get('/')
       .expect(500)
-      .expect(function(res) {
-        console.log(res.text);
-      })
+      .expect('Cache-Control', 'no-cache')
+      .expect(/\<html\>/)
+      .end(done);
+  });
+
+  it('renders custom default error page', function(done) {
+    this.error = Error.http(404, "Not found", {help: "Page not found"});
+    this.options.errorPage = path.resolve(__dirname, "../fixtures/custom-error.ejs");
+
+    supertest(this.server)
+      .get('/')
+      .expect(404)
+      .expect('Cache-Control', 'no-cache')
+      .expect(/\<html\>/)
+      .end(done);
+  });
+
+  it('handles missing error page', function(done) {
+    this.options.errorPage = path.resolve(__dirname, "../fixtures/non-existent.ejs");
+
+    supertest(this.server)
+      .get('/')
+      .expect(500)
+      .expect('Cache-Control', 'no-cache')
+      .expect(/\<html\>/)
+      .end(done);
+  });
+
+  it('handles malformed ejs', function(done) {
+    this.options.errorPage = path.resolve(__dirname, "../fixtures/malformed.ejs");
+
+    supertest(this.server)
+      .get('/')
+      .expect(500)
+      .expect('Cache-Control', 'no-cache')
+      .expect(/\<html\>/)
       .end(done);
   });
 });
