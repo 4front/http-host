6cd4ba5650ba6476866c2d6009b8e7ce5c938392
David Von Lehman <david.vonlehman@gmail.com>
2015-07-10 11:07:24 -0700

    Centralize detection of static asset requests

---
+++ test/is-static-asset.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var isStaticAsset = require('../lib/is-static-asset');
+require('dash-assert');
+
+describe('isStaticAsset', function() {
+  it('anyExceptHtml', function() {
+    assert.isTrue(isStaticAsset.anyExceptHtml({path: '/js/main.js'}));
+    assert.isTrue(isStaticAsset.anyExceptHtml({path: '/img/logo.png'}));
+    assert.isFalse(isStaticAsset.anyExceptHtml({path: '/templates/home.html'}));
+    assert.isFalse(isStaticAsset.anyExceptHtml({path: '/'}));
+    assert.isFalse(isStaticAsset.anyExceptHtml({path: '/blog'}));
+  });
+
+  it('html', function() {
+    assert.isTrue(isStaticAsset.html({path: '/templates/home.html'}));
+    assert.isFalse(isStaticAsset.html({path: '/js/main.js'}));
+  });
+
+  it('htmlXhr', function() {
+    assert.isFalse(isStaticAsset.htmlXhr({path: '/templates/home.html'}));
+    assert.isTrue(isStaticAsset.htmlXhr({path: '/templates/home.html', xhr: true}));
+  });
+
+  it('htmlNotXhr', function() {
+    assert.isTrue(isStaticAsset.htmlNotXhr({path: '/templates/home.html'}));
+    assert.isFalse(isStaticAsset.htmlNotXhr({path: '/templates/home.html', xhr: true}));
+  });
+});
