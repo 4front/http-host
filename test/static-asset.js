a8198cbdd7da0a26572e9c501d2cbac2398a7175
David Von Lehman <david.vonlehman@gmail.com>
2015-11-03 10:28:33 -0800

    Migrate static-asset-redirect functionality to static-asset

--- test/static-asset.js
+++ test/static-asset.js
@@ -1,9 +1,14 @@
 var assert = require('assert');
+var sinon = require('sinon');
+var mime = require('mime');
+var async = require('async');
 var express = require('express');
 var supertest = require('supertest');
 var shortid = require('shortid');
-var urljoin = require('url-join');
+var sbuff = require('simple-bufferstream');
 var staticAsset = require('../lib/middleware/static-asset');
+
+require('dash-assert');
 
 describe('staticAsset', function() {
   var self;
@@ -11,61 +16,140 @@ describe('staticAsset', function() {
   beforeEach(function() {
     self = this;
     this.server = express();
-    this.server.settings.deployedAssetsPath = '/deployments';
+    this.maxAge = 100000;
+    this.responseText = 'OK';
 
-    this.server.settings.deployer = {
-      serve: function(appId, versionId, filePath, res) {
-        res.json({
-          appId: appId,
-          versionId: versionId,
-          filePath: filePath
-        });
-      }
+    this.deployer = this.server.settings.deployer = {
+      serve: sinon.spy(function(appId, versionId, filePath, res) {
+        if (!res.getHeader('cache-control')) {
+          res.setHeader('Cache-Control', 'max-age=' + self.maxAge);
+        }
+
+        res.setHeader('Content-Type', mime.lookup(filePath));
+
+        sbuff(self.responseText).pipe(res);
+      })
     };
 
     this.appId = shortid.generate();
     this.versionId = shortid.generate();
-    this.server.use(staticAsset());
+
+    this.server.get('/deployments/:appId/:versionId/*', staticAsset());
+
+    this.extendedRequest = {
+      virtualEnv: 'production',
+      virtualApp: {appId: self.appId},
+      virtualAppVersion: {versionId: self.versionId}
+    };
+
+    this.server.use(function(req, res, next) {
+      req.ext = self.extendedRequest;
+      next();
+    });
+
+    this.server.get('/*', staticAsset());
 
     this.server.use(function(req, res, next) {
       res.status(404).send('not found');
     });
   });
 
-  it('serves static asset', function(done) {
+  it('serves static asset with max-age', function(done) {
+    var filePath = 'data/ok.text';
     supertest(this.server)
-      .get(urljoin(this.server.settings.deployedAssetsPath, this.appId, this.versionId, 'images/logo.png'))
+      .get('/deployments/' + this.appId + '/' + this.versionId + '/' + filePath)
       .expect(200)
+      .expect('Content-Type', 'text/plain')
+      .expect('Cache-Control', 'max-age=' + self.maxAge)
+      .expect(self.responseText)
       .expect(function(res) {
-        assert.deepEqual(res.body, {
-          appId: self.appId,
-          versionId: self.versionId,
-          filePath: 'images/logo.png'
-        });
+        assert.isFalse(res.headers.etag || false);
+        assert.isTrue(self.deployer.serve.calledWith(self.appId, self.versionId, filePath));
       })
       .end(done);
   });
 
-  // it('skips middleware for non-matching paths', function(done) {
-  //   supertest(this.server)
-  //     .get("images/logo.png")
-  //     .expect(404)
-  //     .end(done);
-  // });
+  it('serves static asset with etag', function(done) {
+    var filePath = 'folder/data.txt';
+    var initialETag;
 
-  it('skips middleware if no file name', function(done) {
+    async.series([
+      function(cb) {
+        supertest(self.server)
+          .get('/' + filePath)
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect('ETag', self.versionId)
+          .expect('Cache-Control', 'no-cache')
+          .expect(self.responseText)
+          .expect(function(res) {
+            initialETag = res.headers.etag;
+            assert.isTrue(self.deployer.serve.calledWith(self.appId, self.versionId, filePath));
+          })
+          .end(cb);
+      },
+      function(cb) {
+        self.deployer.serve.reset();
+        supertest(self.server)
+          .get('/' + filePath)
+          .set('If-None-Match', initialETag)
+          .expect(304)
+          .expect(function(res) {
+            assert.isFalse(self.deployer.serve.called);
+          })
+          .end(cb);
+      },
+      function(cb) {
+        self.deployer.serve.reset();
+        var newVersionId = shortid.generate();
+        self.extendedRequest.virtualAppVersion.versionId = newVersionId;
+        supertest(self.server)
+          .get('/' + filePath)
+          .set('If-None-Match', initialETag)
+          .expect(200)
+          .expect('Content-Type', 'text/plain')
+          .expect('ETag', newVersionId)
+          .expect('Cache-Control', 'no-cache')
+          .expect(self.responseText)
+          .expect(function(res) {
+            assert.isTrue(self.deployer.serve.calledWith(self.appId, newVersionId, filePath));
+          })
+          .end(cb);
+      }
+    ], done);
+  });
+
+  it('skips middleware for non static asset requests', function(done) {
     supertest(this.server)
-      .get(urljoin(this.server.settings.deployedAssetsPath, this.appId, this.versionId))
+      .get('/blog')
       .expect(404)
+      .expect(function(res) {
+        assert.isFalse(self.deployer.serve.called);
+      })
       .end(done);
   });
 
-  // it('skips middleware if deployedAssetsPath does not start with forward slash', function(done) {
-  //   this.server.settings.deployedAssetsPath = "somecdn.com";
-  //
-  //   supertest(this.server)
-  //     .get(urljoin(this.server.settings.deployedAssetsPath, this.appId, this.versionId, "images/logo.png"))
-  //     .expect(404)
-  //     .end(done);
-  // });
+  it('skips middleware for .html requests', function(done) {
+    supertest(this.server)
+      .get('/blog.html')
+      .expect(404)
+      .expect(function(res) {
+        assert.isFalse(self.deployer.serve.called);
+      })
+      .end(done);
+  });
+
+  it('redirects request to localhost for dev sandbox', function(done) {
+    this.extendedRequest.virtualEnv = 'dev';
+    this.extendedRequest.devOptions = { port: 9999 };
+
+    supertest(this.server)
+      .get('/images/photo.png')
+      .expect(302)
+      .expect(function(res) {
+        assert.isFalse(self.deployer.serve.called);
+        assert.equal(res.headers.location, 'http://localhost:9999/images/photo.png');
+      })
+      .end(done);
+  });
 });
